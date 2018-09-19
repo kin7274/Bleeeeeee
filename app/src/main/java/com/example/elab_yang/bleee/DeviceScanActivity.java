38e06144100b984d79c3a233b4fbb4136a1a4378
kin7274 <42366332+kin7274@users.noreply.github.com>
2018-09-19 22:49:43 +0900

    원상복구
    원상복구

--- app/src/main/java/com/example/elab_yang/bleee/DeviceScanActivity.java
+++ app/src/main/java/com/example/elab_yang/bleee/DeviceScanActivity.java
@@ -260,17 +260,17 @@ public class DeviceScanActivity extends ListActivity {
     private BluetoothAdapter.LeScanCallback mLeScanCallback =
             new BluetoothAdapter.LeScanCallback() {
 
-        @Override
-        public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
-            runOnUiThread(new Runnable() {
                 @Override
-                public void run() {
-                    mLeDeviceListAdapter.addDevice(device);
-                    mLeDeviceListAdapter.notifyDataSetChanged();
+                public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mLeDeviceListAdapter.addDevice(device);
+                            mLeDeviceListAdapter.notifyDataSetChanged();
+                        }
+                    });
                 }
-            });
-        }
-    };
+            };
 
     static class ViewHolder {
         TextView deviceName;
